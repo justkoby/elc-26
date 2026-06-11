aab47caf590898c7c96b9cc08b28341758c2bfb4
justkoby <justkoby@github.com>
2026-06-11 13:51:56 +0000

    fix: remove broken/unwanted partner logos and adjust mof logo size

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -4,8 +4,6 @@ const Partners = () => {
   const logos = [
     { src: '/UN.png', alt: 'United Nations' },
     { src: '/mof.png', alt: 'Ministry of Finance' },
-    { src: '/britishcouncillog.jpg', alt: 'British Council' },
-    { src: '/aasulogo.png', alt: 'All-Africa Students Union (AASU)' },
     { src: '/ydi.png', alt: 'Youth Diplomacy Institute' },
     { src: '/sdgss.png', alt: 'SDG Summer School' },
     { src: '/maucn.jpeg', alt: 'MAUCN' }
